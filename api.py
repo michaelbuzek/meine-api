8247bb134189b63d06c1fc4c9f631f6ae78357c0
michaelbuzek <115479993+michaelbuzek@users.noreply.github.com>
2025-04-11 15:19:03 +0200

    Update api.py wlan und setup_type

--- api.py
+++ api.py
@@ -11,6 +11,8 @@ def create_user():
     email = data.get('email')
     setup_name = data.get('setup_name')
     cpe = data.get('cpe')
+    wlan = data.get('wlan')
+    setup_type = data.get('setup_type')
 
     if not name or not email:
         return jsonify({"error": "Name und E-Mail sind erforderlich"}), 400
@@ -19,7 +21,9 @@ def create_user():
         name=name,
         email=email,
         setup_name=setup_name,
-        cpe=cpe
+        cpe=cpe,
+        wlan=wlan,
+        setup_type=setup_type
     )
 
     db.session.add(user)
@@ -32,7 +36,9 @@ def create_user():
             "name": user.name,
             "email": user.email,
             "setup_name": user.setup_name,
-            "cpe": user.cpe
+            "cpe": user.cpe,
+            "wlan": user.wlan,
+            "setup_type": user.setup_type
         }
     }), 201
 
@@ -46,7 +52,9 @@ def list_users():
             "name": u.name,
             "email": u.email,
             "setup_name": u.setup_name,
-            "cpe": u.cpe
+            "cpe": u.cpe,
+            "wlan": u.wlan,
+            "setup_type": u.setup_type
         }
         for u in users
     ])
@@ -63,21 +71,10 @@ def get_user(user_id):
         "name": user.name,
         "email": user.email,
         "setup_name": user.setup_name,
-        "cpe": user.cpe
+        "cpe": user.cpe,
+        "wlan": user.wlan,
+        "setup_type": user.setup_type
     })
-
-# DELETE /users/<id> – Benutzer löschen
-@api_bp.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "User nicht gefunden"}), 404
-
-    db.session.delete(user)
-    db.session.commit()
-
-    return jsonify({"message": f"User mit ID {user_id} wurde gelöscht."})
-
 
 # PUT /users/<id> – Benutzer aktualisieren
 @api_bp.route('/users/<int:user_id>', methods=['PUT'])
@@ -92,6 +89,8 @@ def update_user(user_id):
     user.email = data.get('email', user.email)
     user.setup_name = data.get('setup_name', user.setup_name)
     user.cpe = data.get('cpe', user.cpe)
+    user.wlan = data.get('wlan', user.wlan)
+    user.setup_type = data.get('setup_type', user.setup_type)
 
     db.session.commit()
 
@@ -102,7 +101,20 @@ def update_user(user_id):
             "name": user.name,
             "email": user.email,
             "setup_name": user.setup_name,
-            "cpe": user.cpe
+            "cpe": user.cpe,
+            "wlan": user.wlan,
+            "setup_type": user.setup_type
         }
     })
 
+# DELETE /users/<id> – Benutzer löschen
+@api_bp.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User nicht gefunden"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"message": f"User mit ID {user_id} wurde gelöscht."})
